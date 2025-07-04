395214931632b61ada1df7827defdfdf1674714a
Grant Codes <grntcodes@users.noreply.github.com>
2025-07-04 07:13:06 -0400

    refactor: migrate to React 18

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
 // Update at 2025-05-04 16:07:16: docs: update README with setup instructions
 // Update at 2025-04-16 08:48:54: test: increase test coverage to 90%
 // Update at 2025-04-08 12:07:03: docs: add contributing guidelines
+// Update at 2025-07-04 07:13:06: refactor: improve accessibility
