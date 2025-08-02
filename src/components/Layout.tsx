8a216dfdab7dd556401772e1ace4e5df9b2ab910
Grant Codes <grntcodes@users.noreply.github.com>
2025-08-02 06:41:07 -0400

    refactor: migrate to React 18

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,3 +2,4 @@
 // Update at 2025-04-16 08:48:54: test: increase test coverage to 90%
 // Update at 2025-04-08 12:07:03: docs: add contributing guidelines
 // Update at 2025-07-04 07:13:06: refactor: improve accessibility
+// Update at 2025-08-02 06:41:07: fix: resolve hydration issues in SSR
